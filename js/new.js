d26dfec546099510f44319b0a532ab94c688e450
Zhengyi Ma <1204056646@qq.com>
2023-03-10 12:45:25 +0800

    feat: 实现new操作符

---
+++ js/new.js
@@ -0,0 +1,16 @@
+// 实现关键字new：new Array(argus)
+
+function myNew(constructor, argus) {
+    // 声明一个空对象
+    const obj = {}
+    // 构造函数的原型指向空对象的原型
+    obj.__proto__ = constructor.prototype
+    // 执行构造函数，并把this指向空对象
+    const res = constructor.apply(obj, argus)
+
+    // 返回构造函数指向的结果
+    return res instanceof Object ? res : obj
+}
+
+
+
