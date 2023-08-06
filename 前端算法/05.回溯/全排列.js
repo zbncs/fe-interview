1e82747e44a274b7e63b11c465366c0c7fb9339c
Zhengyi Ma <1204056646@qq.com>
2023-08-06 09:06:21 +0800

    feat：new vue

--- 前端算法/05.回溯/全排列.js
+++ 前端算法/05.回溯/全排列.js
@@ -1,3 +1,34 @@
 // https://leetcode.cn/problems/permutations/
 
 
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+  const res = [];
+  const temp = [];
+
+  const fn = (temp, nums) => {
+      if (temp.length === nums.length) {
+          res.push([...temp]);
+          return;
+      }
+
+      for (let i = 0; i < nums.length; i++) {
+          if (temp.indexOf(nums[i]) > -1) continue;
+          
+          temp.push(nums[i]);
+          fn(temp, nums);
+          temp.pop();
+      }
+  }
+
+  fn(temp, nums);
+
+  return res;
+};
+// [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+const nums = [1,2,3];
+permute(nums);
+
---
+++ 前端算法/05.回溯/单词搜索.js
@@ -0,0 +1,5 @@
+// https://leetcode.cn/problems/word-search/
+
+
+
+
